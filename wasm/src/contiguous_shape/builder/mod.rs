ef40e835a18e5d856d6f76d7ba3f09af92afa9bf
JM <j.maxwell.alan@gmail.com>
2022-12-31 23:22:26 -0500

    add interesct type to csb

--- wasm/src/contiguous_shape/builder/mod.rs
+++ wasm/src/contiguous_shape/builder/mod.rs
@@ -1,18 +1,20 @@
 mod anonymous_lifetime_impls;
+mod contiguous_shape_from_contiguous_shape_builder;
 mod named_lifetime_impls;
 
 use crate::{
-  canvas_point::CanvasPoint, shape_config::ShapeConfig,
-  shape_segment::ShapeSegment, stroke_batch::StrokeBatch,
+  canvas_point::CanvasPoint, enums::intersect_type::IntersectType,
+  shape_config::ShapeConfig, shape_segment::ShapeSegment,
+  stroke_batch::StrokeBatch,
 };
 
-#[derive(Debug, Default)]
 pub struct ContiguousShapeBuilder<'batch_life: 'builder_life, 'builder_life> {
-  pub batch: Option<&'builder_life mut StrokeBatch<'batch_life>>,
+  pub batch: &'builder_life mut StrokeBatch<'batch_life>,
   pub start: CanvasPoint,
   pub segments: Vec<ShapeSegment>,
   pub closed_shape: bool,
   pub filled_shape: bool,
+  pub intersect_type: IntersectType,
   pub config: BuilderConfig,
 }
 
