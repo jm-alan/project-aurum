73785d78079e9265142d79f143f620089c578c95
JM <j.maxwell.alan@gmail.com>
2023-01-07 22:13:28 -0500

    create canvas point distance resolver

--- wasm/src/canvas_point/mod.rs
+++ wasm/src/canvas_point/mod.rs
@@ -5,3 +5,9 @@ pub struct CanvasPoint {
   pub x: f64,
   pub y: f64,
 }
+
+impl CanvasPoint {
+  pub fn distance_to(&self, other: CanvasPoint) -> f64 {
+    ((self.x - other.x).powf(2.0) + (self.y - other.y).powf(2.0)).sqrt()
+  }
+}
