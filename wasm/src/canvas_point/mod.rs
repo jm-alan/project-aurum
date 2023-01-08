fb0eca2cbbc60424b8bfc54d84a2725d412675c0
JM <j.maxwell.alan@gmail.com>
2023-01-07 23:42:03 -0500

    create angle_to canvaspoint helper function

--- wasm/src/canvas_point/mod.rs
+++ wasm/src/canvas_point/mod.rs
@@ -8,6 +8,26 @@ pub struct CanvasPoint {
 
 impl CanvasPoint {
   pub fn distance_to(&self, other: CanvasPoint) -> f64 {
-    f64::hypot(self.x - other.x, self.y - other.y)
+    f64::hypot(other.x - self.x, other.y - self.y)
+  }
+
+  pub fn angle_to(&self, other: CanvasPoint) -> f64 {
+    let x_diff = other.x - self.x;
+    let y_diff = other.y - self.y;
+    match &(
+      x_diff < 0.0,
+      x_diff == 0.0,
+      x_diff > 0.0,
+      y_diff < 0.0,
+      y_diff == 0.0,
+      y_diff > 0.0,
+    ) {
+      (_, true, _, _, true, _) => 0.0,
+      (true, _, _, _, true, _) => std::f64::consts::PI,
+      (_, true, _, true, _, _) => std::f64::consts::PI * 1.5,
+      (_, true, _, _, _, true) => std::f64::consts::PI * 0.5,
+      (true, _, _, _, _, _) => -1.0 * (x_diff / y_diff).atan(),
+      _ => (x_diff / y_diff).atan(),
+    }
   }
 }
